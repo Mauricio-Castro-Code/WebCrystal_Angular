09c22c9240c9517c7ff07b16f9c0a9e83ab7216b
Mauricio Castro Valencia <mauriciocv@Mauricios-MacBook-Air.local>
2026-04-03 20:54:05 -0600

    feat: add @vercel/speed-insights integration to enhance performance monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject as injectAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
 injectAnalytics();
+injectSpeedInsights();
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
